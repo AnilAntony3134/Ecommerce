0a3e5d2c4f94f37e28927ef07a24a0b06c2c5742
Anil Antony <anilantony3134@gmail.com>
2022-05-06 20:47:37 +0530

    Animation Added (Still working)

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styledComponents from 'styled-components';
 import {popularProducts} from "./data";
 import Product from './Product';
+// import { AOS } from 'aos';
+// import "aos/dist/aos.css";
 
 const Container = styledComponents.div`
     padding: 25px;
@@ -11,8 +13,13 @@ const Container = styledComponents.div`
 `
 
 const Products = () => {
+    // useEffect(()=>{
+    //     AOS.init({ duration: 2000 });
+    // },[]);
   return (
-    <Container>
+    <Container
+    //  data-aos="fade-up"
+     >
         {popularProducts.map(item=>(
              <Product item={item} key={item.id} />
         ))
